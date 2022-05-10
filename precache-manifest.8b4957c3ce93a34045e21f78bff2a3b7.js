4ad6931ac3c582423516ba46580c85f3af78af55
Taleh Rzayev <taleh.rzayev.i@asoiu.edu.az>
2022-05-10 17:27:04 +0400

    Add files via upload

---
+++ precache-manifest.8b4957c3ce93a34045e21f78bff2a3b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b38742bb1d497a8a43e9db188d31a28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a574ffeee3d9d7ef669",
+    "url": "/static/css/main.a9ffa556.chunk.css"
+  },
+  {
+    "revision": "02b059dcb659e950ca39",
+    "url": "/static/js/2.5c63a63f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5c63a63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a574ffeee3d9d7ef669",
+    "url": "/static/js/main.ffecf26d.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "2dcb4821b18b3e6060236b90b13c136b",
+    "url": "/static/media/hoaxify.2dcb4821.png"
+  },
+  {
+    "revision": "926fda932fcddf9dae79b8a3a560745b",
+    "url": "/static/media/profile.926fda93.png"
+  }
+]);
